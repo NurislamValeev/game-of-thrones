5cbd9c2be9ce26a497c00c9d1745cdb866e1fe61
Nurislam <nurislamvaleev@gmail.com>
2021-07-03 17:38:49 +0300

    added services, added randomChar

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,32 +1,62 @@
-import React, {Component} from 'react';
-import './randomChar.css';
+import React, {Component} from 'react'
+import './randomChar.css'
+import GotService from "../../services/gotService";
 
 export default class RandomChar extends Component {
+   constructor(props) {
+      super(props)
+      this.updateChar()
+   }
 
-    render() {
+   GotService = new GotService()
 
-        return (
-            <div className="random-block rounded">
-                <h4>Random Character: John</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender </span>
-                        <span>male</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born </span>
-                        <span>11.03.1039</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died </span>
-                        <span>13.09.1089</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture </span>
-                        <span>Anarchy</span>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+   state = {
+      name: null,
+      gender: null,
+      born: null,
+      died: null,
+      culture: null
+   }
+
+   updateChar() {
+      const id = 100
+      this.GotService.getCharacter(id)
+         .then((char) => {
+            this.setState({
+               name: char.name,
+               gender: char.gender,
+               born: char.born,
+               died: char.died,
+               culture: char.culture
+            })
+         })
+   }
+
+   render() {
+      const {name, gender, born, died, culture} = this.state
+
+      return (
+         <div className="random-block rounded">
+            <h4>Random Character: {name}</h4>
+            <ul className="list-group list-group-flush">
+               <li className="list-group-item d-flex justify-content-between">
+                  <span className="term">Gender </span>
+                  <span>{gender}</span>
+               </li>
+               <li className="list-group-item d-flex justify-content-between">
+                  <span className="term">Born </span>
+                  <span>{born}</span>
+               </li>
+               <li className="list-group-item d-flex justify-content-between">
+                  <span className="term">Died </span>
+                  <span>{died}</span>
+               </li>
+               <li className="list-group-item d-flex justify-content-between">
+                  <span className="term">Culture </span>
+                  <span>{culture}</span>
+               </li>
+            </ul>
+         </div>
+      );
+   }
 }
